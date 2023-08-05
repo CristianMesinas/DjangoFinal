3d8f4e5e3db9f26553108b6203f77404502e19ce
Cristian Mesina <mesina_02@hotmail.com>
2023-08-05 01:11:29 -0300

    Crear y editar con formulario

--- inventarioF/views.py
+++ inventarioF/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render, redirect #hay que importar los distintos metodos a utilizar, de lo contrario no los reconoce
 from django.http import HttpResponse
 from .models import Productos, Categorias
+from .forms import ProductoForm #para saber si esta bien creado, nos posicionamos sobre el elemento y apretando ctrl y clik deberia llevarnos a la pagina
+                                #en este caso el .forms nos envia a forms.py con el ctrl click
 
 # Create your views here.
 def listado(request):
-    productos = Productos.objects.all() #trae todos los productos
+    productos = Productos.objects.all().order_by('nombre') #trae todos los productos, el .order_by('nombre') los ordena de forma descendente por nombre, si quiero asc se pone -nombre
     categoria = Categorias.objects.all() #todos los objetos de Categorias se cargan en categoria
-    context = {"productos": productos, "categorias":categoria} # con el context le decimos a django como representar mis productos y categorias en HTML
+    formulario = ProductoForm()
+    context = {"productos": productos, "categorias":categoria, "formulario": formulario} # con el context le decimos a django como representar mis productos y categorias en HTML
     return render(request, 'listado.html', context)
 
 def crearProducto(request): # creamos el identificador para poder cargar nuevos datos
-    nombre1 = request.POST ["nombre"] # "nombre" es tomado del ID usado en el modal para identificar que dato vamos a cargar
-    precio1 = request.POST ["precio"] # "precio" es tomado del ID usado en el modal para identificar que dato vamos a cargar
-    stock1 = request.POST ["stock"]
-    categoria1 = request.POST ["categoria"]
-    catego = Categorias.objects.get(id= categoria1) #traiga el objeto que selecciono en el select
-    producto = Productos(nombre= nombre1, precio = precio1, stock = stock1, categoria = catego) #Nombre Precio y Stock provienen del models.py
-    producto.save() #el .save se utiliza para guardar el producto en mi base de datos
-    return redirect('/') #el redirect cuando se hace un return permite redireccionar
+#    nombre1 = request.POST ["nombre"] # "nombre" es tomado del ID usado en el modal para identificar que dato vamos a cargar
+#    precio1 = request.POST ["precio"] # "precio" es tomado del ID usado en el modal para identificar que dato vamos a cargar
+#    stock1 = request.POST ["stock"]
+#    categoria1 = request.POST ["categoria"]
+#    catego = Categorias.objects.get(id= categoria1) #traiga el objeto que selecciono en el select
+#    producto = Productos(nombre= nombre1, precio = precio1, stock = stock1, categoria = catego) #Nombre Precio y Stock provienen del models.py
+#    producto.save() #el .save se utiliza para guardar el producto en mi base de datos
+#    return redirect('/') #el redirect cuando se hace un return permite redireccionar
                          #a la url que se le indique en este caso la BASE
+
+    # Asi es como se usa el form, para guardar un producto
+    formulario = ProductoForm(request.POST) #con este tipo de form se ahorra muchas lineas de codigo y se obtienen mismos resultados
+    if formulario.is_valid(): #con el .is_valid se valida el FORM, antes de guardarlo
+        formulario.save()
+        return redirect('/')
 
 def editarProducto(request, id): #los id son para referenciar que producto editar/eliminar
 #se puede poner tambien id_producto o prod_id o de la forma que queramos
